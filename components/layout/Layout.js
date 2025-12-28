3f2ce2952443ce010121ee3dbb767b89539e3fb2
Parsa Mehrpooshan <parsamehrpooshan@gmail.com>
2025-12-28 15:11:58 +0330

    initial commit

---
+++ components/layout/Layout.js
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import styles from "./Layout.module.css";
+
+function Layout({ children }) {
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.left}>
+          <Link href="/">BotoFood</Link>
+        </div>
+        <div className={styles.right}>
+          <Link href="/menu">Menu</Link>
+          <Link href="/categories">Categories</Link>
+        </div>
+      </header>
+      <div className={styles.container}>{children}</div>
+      <footer className={styles.footer}>
+        developed by
+        <a
+          href="https://github.com/parsamehrpooshan"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Parsa Mehrpooshan
+        </a> |
+        <a href="https://botostart.ir" target="_blank" rel="noreferrer">
+          Botostart
+        </a>
+        Next.js course | Botostart Project &copy;
+      </footer>
+    </>
+  );
+}
+
+export default Layout;
